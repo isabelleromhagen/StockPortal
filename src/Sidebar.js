b8f95034f4bf6e63debb62354a11bb4aa716829b
91matfri <mattiasfrigren@hotmail.com>
2020-06-03 12:22:31 +0200

    update sidebars

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,26 +2,24 @@ import React from 'react';
 import './styling/SideBar.css' 
 import LinkComponent from './component/LinkComp';
 
-const linkNameArr =['Hem','Min portfölj','Inställingar','Logga ut'];
-
 class SideBar extends React.Component {
 
     constructor(props){
         super(props)
 
         this.state ={
-            links:[linkNameArr.length]
         }
+        this.dashboard =this.dashboard.bind(this);
     }
 
-    getLinks(){
-        links = linkNameArr.map()
+    dashboard(){
+        console.log('dashboard');
     }
 
     render(){
         return(
-            <div class="sidediv">
-              <a className ="sidbar" href="dashboard">Hem</a>
+            <div className="sidediv">
+              <a className ="sidbar" onClick={this.dashboard}>Hem</a>
               <a className ="sidbar" href="myPortfolio">Min Portfölj</a>
               <a className ="sidbar" href="#settings">Inställningar</a>
               <a className ="sidbar" href="logout">Logga ut</a>
