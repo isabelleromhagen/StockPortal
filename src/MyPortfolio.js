970a2b957b78190da4e3d85455b1ef86babeabe4
81khameh <khazar.mehraban@gmail.com>
2020-06-10 16:05:30 +0200

    added no holdings to show function

--- src/MyPortfolio.js
+++ src/MyPortfolio.js
@@ -15,11 +15,11 @@ const updateDate = () => {
     let month = newDate.getMonth() + 1;
     let monthHuman = (month <= 9) ? "0" + month : month;
     let year = newDate.getFullYear();
-    let hour = newDate.getHours();
+    let hour = newDate.getHours();    
     let min = newDate.getMinutes();
     let minHuman = (min <= 9) ? "0" + min : min;
     return (
-        //   `Latest update ${this.year}-${this.monthHuman}-${this.dateHuman} ${this.hour}:${this.minHuman}`
+        //   `LaStocks update ${this.year}-${this.monthHuman}-${this.dateHuman} ${this.hour}:${this.minHuman}`
         ("Senast uppdaterat " + year + "-" + monthHuman + "-" + dateHuman + "  " + hour + ":" + minHuman)
     );
 }
@@ -27,8 +27,8 @@ const updateDate = () => {
 const tableHeaderList = (index) => (headerTitleList.map((elem) => <th key={index}>{elem}</th>));
 
 const MyPortfolio = ({ CompanyData }) => {
-    const [isInlogged,setIsinlogged ]=useState(true); //TODO fix the metod for login
-    const [latestUpdate, getLatestUpdate] = useState(updateDate);
+    const [isInlogged,setIsinlogged ]=useState(false); //TODO fix the metod for login
+    const [laStocksUpdate, getLaStocksUpdate] = useState(updateDate);
     const [items, setItems] = useState([]);
     const [header] = useState(tableHeaderList());
     const [page, setPage] = useState(0);
@@ -38,18 +38,33 @@ const MyPortfolio = ({ CompanyData }) => {
         setRowsPerPage(parseInt(e.target.value, amountToshow));
         setPage(0);
     };
-    
+    const currentItems = items.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
+  
     useEffect(() => {
         setItems(isInlogged ? CompanyData: [])
       },[]);
-    
+  
 
+const Stocks=()=>{ return (currentItems.map((elem, index) =>
+    <PortfolioTable
+        key={index}
+        company={elem.company}
+        holdingValue={elem.holdingValue}
+        type={elem.type}
+        holdingAmount={elem.holdingAmount}
+        stockNumber={elem.stockNumber}
+        owns={elem.owns}
+        voteValue={elem.voteValue}
+        ClassName={"downloadBtn"}
+        btnID={index}
+        btnIcon={"fa fa-download"}
+    />)
+)};
     
-    const currentItems = items.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
 
     return (
         <div className="background">
-           <div id="header"> <h4 > {site}  </h4> | <p id="headerText">{latestUpdate}</p></div>
+           <div id="header"> <h4 > {site}  </h4> | <p id="headerText">{laStocksUpdate}</p></div>
             <div className="portFolio-container">
 
                 <table id="portfolio-table">
@@ -59,23 +74,9 @@ const MyPortfolio = ({ CompanyData }) => {
                         </tr>
                     </thead>
                     <tbody>
+                    {items.length>0 ? <Stocks/>: <p>Du har inget innehav</p> }
 
-                        {currentItems.map((elem, index) =>
-                            <PortfolioTable
-
-                                key={index}
-                                company={elem.company}
-                                holdingValue={elem.holdingValue}
-                                type={elem.type}
-                                holdingAmount={elem.holdingAmount}
-                                stockNumber={elem.stockNumber}
-                                owns={elem.owns}
-                                voteValue={elem.voteValue}
-                                ClassName={"downloadBtn"}
-                                btnID={index}
-                                btnIcon={"fa fa-download"}
-                            />
-                        )}
+                       
                     </tbody>
                 </table>
 
