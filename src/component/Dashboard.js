cf86cb8f6db012fe2c090062f3aa4cf7185c747c
isabelleromhagen <isabelle.romhagen@gmail.com>
2020-06-10 18:53:20 +0200

    converted classes to hooks

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -1,261 +1,183 @@
-import React, { Component } from "react";
+import React, {useState} from "react";
 import ButtonComponent from "./ButtonComp";
 import image from "../images/cymbal.jpg"
 import '../styling/Dashboard.css'
 
-
-class User {
-    constructor(name, pNum, phone, email, address, zip, city, hasStocks, latestUpdate){
-        this.name = name;
-        this.welcomeMsg = this.getWelcomeMsg;
-        this.pNum = pNum;
-        this.phone = phone;
-        this.email = email;
-        this.address = address;
-        this.zip = zip;
-        this.city = city;
-        this.hasStocks = hasStocks;
-        this.latestUpdate = latestUpdate;
-
-    }
-    getWelcomeMsg() {
-        if(!this.hasStocks) {
-            return `Välkommen ${this.name}! 
-            Du har inte något innehav ännu. Du får ett mail så fort det är uppdaterat!`;
-        } else {
-            return `Välkommen ${this.name}! Ditt innehav blev senast uppdaterat ${this.latestUpdate}.
-            Ta gärna en titt!`
-        }
-    }
-}
-
-const testUser = new User(
-    'Isabelle Romhagen', '940319-1234', '079 111 2222', 'isabelle.romhagen@gmail.com',
-     'Lantmilsgatan 7', '41501', 'Göteborg', true, '2020-06-09');
-// console.log(testUser);
-
-class Dashboard extends Component {
-    constructor(props) {
-        super(props);
-        this.user = testUser
-        this.welcomeMsg = this.user.getWelcomeMsg();
-        
-
-        this.state = {
-            
-            
-        }
-    }
-    
-    render() {
-    
-        return(
-            <div>
-                <Banner  welcomeMsg={this.welcomeMsg}/>
-                <DashboardContainer/>
-            </div>
-        );
-    }    
-}
-
-
-  
-
-const Banner = ({welcomeMsg}) => {
-    return(
-        <p className="welcomeBanner">{welcomeMsg}</p>
-    );
-}
-
-class DashboardContainer extends Component {
-    render() {
-        return(  
-            <div id="dashboardContainer">
-                <Profile/>
-                <Shareholding/>
-            </div>
-        );
+const Dashboard = () => {
+    const [firstName, setFirstName] = useState('anonym');
+    const [lastName, setLastName] = useState('anonym');
+    const [welcomeMsg, setWelcomeMessage] = useState(`Välkommen ${firstName}! 
+        Du har inte något innehav ännu. Du får ett mail så fort det är uppdaterat!`);
+    const [pNum, setPNum] = useState('-');
+    const [phone, setPhone] = useState('-');
+    const [email, setEmail] = useState('-');
+    const [address, setAddress] = useState('-');
+    const [zip, setZip] = useState('-');
+    const [city, setCity] = useState('-');
+    const [hasStocks, setHasStocks] = useState(false);
+    const [latestUpdate, setLatestUpdate] = useState('-');
+   
+    const Banner = ({welcomeMsg}) => {
+        return<p className="welcomeBanner">{welcomeMsg}</p>
     }
 
-}
-
-class Profile extends Component {
-    constructor(props) {
-        super(props);
-
-        this.user = testUser;
-
-        this.state = {
-            industries: ["Industry 1", "Industry 2", "Industry 3", "Industry 4"]
+    const Profile = () => {
+        const [industries, setIndustries] = useState([]);
+        const SuggestedInd = () => {
+            if(industries.length > 0) {
+                    return(
+                        <div>
+                            {industries.map((industry, index) => <p id={index} industryname={industry} key={index}>{industry}</p>)}
+                        </div>
+                    );
+            } else {
+                return <p>Inga industrier valda än</p>
+            }
         };
-    }
-    render() {
-      
         return(
             <div id="profileDiv" className="container">
-                <h4 className="dashboardSubtitle">Min profil</h4>
-                <ButtonComponent btnClassName="dashboardBtn" btnName={"Redigera"}/>
-                <div className="content">
-                    <img src={image} alt="profile pic"/>
-                    <PersonalData name={this.user.name} pNum={this.user.pNum}/>
-                    <div id="industryDiv">
-                        <p className="profileSubtitle">Föreslagna industrier</p>
-                        <SuggestedInd className="industry" industries={this.state.industries}/>
+                    <h4 className="dashboardSubtitle">Min profil</h4>
+                    <ButtonComponent btnClassName="dashboardBtn" btnName={"Redigera"}/>
+                    <div className="content">
+                        <img src={image} alt="profile pic"/>
+                        <PersonalData name={firstName} pNum={pNum}/>
+                        <div id="industryDiv">
+                            <p className="profileSubtitle">Föreslagna industrier</p>
+                            <SuggestedInd className="industry" industries={industries}/>
+                        </div>
+                        <Contact phone={phone} email={email} address={address} zip={zip} city={city}/>
                     </div>
-                    <Contact phone={this.user.phone} email={this.user.email} address={this.user.address} zip={this.user.zip} city={this.user.city}/>
                 </div>
+        );
+    }
+
+    const PersonalData = () => {
+        return(
+            <div id="personalData">
+                <h4>{firstName} {lastName}</h4>
+                <p>Person-/organisationsnummer</p>
+                <p>{pNum}</p>
+            </div> 
+        );
+    }
+
+    const Contact = () => {
+        return(
+            <div id="contact">
+                <p className="profileSubtitle">Kontaktuppgifter</p> 
+                <table>
+                    <tbody>
+                                <tr>
+                                    <th>Telefon</th>
+                                    <td>{phone}</td>
+                                </tr>
+                                <tr>
+                                    <th>Mail</th>
+                                    <td>{email}</td>
+                                </tr>
+                                <tr>
+                                    <th>Adress</th>
+                                    <td>{address}</td>
+                                </tr>
+                                <tr>
+                                    <th>Postnummer</th>
+                                    <td>{zip}</td>
+                                </tr>
+                                <tr>
+                                    <th>Postort</th>
+                                    <td>{city}</td>
+                                </tr>
+                    </tbody>     
+                </table>
             </div>
         );
     }
-}
+    
+    const Shareholding = () => {
+        const [shares, setShares] = useState([
+            ]);
 
-
-const PersonalData = ({name, pNum}) => {
-    return(
-        <div id="personalData">
-            <h4>{name}</h4>
-            <p>Person-/organisationsnummer</p>
-            <p>{pNum}</p>
-        </div> 
-    );
-}
-
-const SuggestedInd = ({industries}) => {
-    return(
-        <div>
-            {
-                industries.map((industry, index) => <p id={index} industryname={industry} key={index}>{industry}</p>)
+        let getTotalValue = () => {
+                if(shares.length > 0) {
+                    return shares.map((item) => {
+                        return item.value;
+                    }).reduce((total, value) => {
+                        return total + value;
+                    });
+                } else {
+                    return 0;
+                }              
             }
-        </div>
-    );
-}
-
-
-const Contact = ({phone, email, address, zip, city}) => {
-    return(
-        <div id="contact">
-            <p className="profileSubtitle">Kontaktuppgifter</p> 
-            <table>
-               <tbody>
-                        <tr>
-                            <th>Telefon</th>
-                            <td>{phone}</td>
-                        </tr>
-                        <tr>
-                            <th>Mail</th>
-                            <td>{email}</td>
-                        </tr>
-                        <tr>
-                            <th>Adress</th>
-                            <td>{address}</td>
-                        </tr>
-                        <tr>
-                            <th>Postnummer</th>
-                            <td>{zip}</td>
-                        </tr>
-                        <tr>
-                            <th>Postort</th>
-                            <td>{city}</td>
-                        </tr>
-                
-               </tbody>
-                   
-            </table>
-        </div>
-    );
-}
-
-
-class Shareholding extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            
-            shares: [{
-                color: 'blue',
-                sectorname: "Byggsektorn",
-                companynames: ['Företag 1', 'företag 2'],
-                value: 32244 
-            },
-            {
-                color: 'green',
-                sectorname: "Material och råvaror",
-                companynames: ['Företag a', 'företag b'],
-                value: 10000
-            },
-            {
-                color: 'red',
-                sectorname: "Läkemedel",
-                companynames: ['Företag a', 'företag b'],
-                value: 12345
-            }
-
-        ],
-            
-        }
-
-    }
-
-    getTotalValue = () => {
-       if(this.state.shares) {
-     
-        return this.state.shares.map((item) => {
-            return item.value;
-        }).reduce((total, value) => {
-            return total + value;
-        });
-       } else {
-           return 0;
-       }
-        
-    }
-  
-    render() {
-        
         return(
             <div id="propertyDiv" className="container">
                 <h4 className="dashboardSubtitle">Mitt innehav</h4>
                 <ButtonComponent btnClassName="dashboardBtn" btnName={"Min Portfölj"}/>
                 <div className="content">
-                    <h1>{this.getTotalValue()} SEK</h1>
-                     {/*TODO: find out how to tell CSS what colors should be shown and how much of each*/}
-                    <ShareInfo shares={this.state.shares}/>
+                    <h1>{getTotalValue()} SEK</h1>
+                    <ShareInfo shares={shares}/>
                 </div>
             </div>
         );
     }
-}
 
- 
-
-
-const ShareInfo = ({shares}) => {
-    if(shares) {
-        return(
-            <div>
-            <div id="barDiagram"></div>
-                {
-                    shares.map((share, index) => 
-                    <div key={index}>
-                        <div color={shares[index].color}></div>
-                        <p>{share.sectorname}</p>
-                        <p>{share.companynames}</p>
-                        <p>{share.value}</p>
-                    </div>)
-                }
-            </div>
-        );
+    const ShareInfo = ({shares}) => {
+        if(shares.length > 0) {
+            return(
+                <div>
+                <div id="barDiagram"></div>
+                    {shares.map((share, index) => 
+                        <div key={index}>
+                            <div color={shares[index].color}></div>
+                            <p>{share.sectorname}</p>
+                            <p>{share.companynames}</p>
+                            <p>{share.value}</p>
+                        </div>)}
+                </div>
+            );
+        }
+        else {
+            return(
+                <div>
+                    <h2>Inget innehav tillagt ännu</h2>
+                </div>
+            );
+        }
     }
-    else {
-        return(
-            <div>
-                <h2>Inget innehav tillagt ännu</h2>
-            </div>
-        );
-    }
-  
-}
-
+    return(
+        <div>
+            <Banner welcomeMsg={welcomeMsg}/>
+            <Profile/>
+            <Shareholding/>
+        </div>
+    );
+};
 
 export default Dashboard;
+
+//***********************TEST DATA **************************//
+
+
+// const testUser = new User(
+//     'Isabelle Romhagen', '940319-1234', '079 111 2222', 'isabelle.romhagen@gmail.com',
+//      'Lantmilsgatan 7', '41501', 'Göteborg', true, '2020-06-09');
+// console.log(testUser);
+
+
+// {
+//     color: 'blue',
+//     sectorname: "Byggsektorn",
+//     companynames: ['Företag 1', 'företag 2'],
+//     value: 32244 
+// },
+// {
+//     color: 'green',
+//     sectorname: "Material och råvaror",
+//     companynames: ['Företag a', 'företag b'],
+//     value: 10000
+// },
+// {
+//     color: 'red',
+//     sectorname: "Läkemedel",
+//     companynames: ['Företag a', 'företag b'],
+//     value: 12345
+// }
