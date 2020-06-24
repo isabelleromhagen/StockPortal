efd5b04624c4a7c786273678c000953533417de1
isabelleromhagen <isabelle.romhagen@gmail.com>
2020-06-24 10:54:13 +0200

    removed redundant reference to mock data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './styling/App.css';
 import Routes from './loggedIn/sidebar/Sidebar';
-import CompanyData from './data/mockData.json';
+
 
 function App() {
   return (
